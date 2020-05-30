20cb239abc3ae629633fb5050f16974ccb3b69b5
Clayton Jones <claytonjonesLMP@gmail.com>
2020-05-30 16:44:52 -0700

    added printChar function to log the character selection

--- readline/commands/charDelete.js
+++ readline/commands/charDelete.js
@@ -9,8 +9,17 @@ const rl = require('../readline');
 async function deleteCharacter(character) {
   return new Promise( async (resolve, reject) => {
     try {
-      let response = await superagent.delete(`https://cf-dnd-character-creator.herokuapp.com/v1/api/character/${character._id}`);
-      console.log(`Your character, ${character.name}, has been claimed by ${character.deity}!`);
+      let confirm = await rl.ask(`Confirm deletion of ${character.name}? (Y/n)`);
+
+      if (confirm.toUpperCase() === 'Y') {
+        await superagent.delete(`https://cf-dnd-character-creator.herokuapp.com/v1/api/character/${character._id}`);
+        console.log(`Your character, ${character.name}, has been claimed by ${character.deity}!`);
+      } else if (confirm.toUpperCase() === 'N') {
+        console.log(`${character.name} lives to fight another day.`);
+      } else {
+        console.log('Command not recognized. Returning to main menu...');
+      }
+
       resolve();
     }
     catch(e){
