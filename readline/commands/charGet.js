597491adde5e1a28b91bb91352dcd8b79550bcd3
Madison Stehle <mnstehle3@gmail.com>
2020-06-01 19:16:15 -0700

    changed coloring of character view

--- readline/commands/charGet.js
+++ readline/commands/charGet.js
@@ -3,6 +3,8 @@
 // chalk is for customizing terminal text
 const chalk = require('chalk');
 
+const gradient = require('gradient-string');
+
 // superagent is used to make requests to external APIs
 const superagent = require('superagent');
 
@@ -49,7 +51,7 @@ async function charGet(user) {
         }
 
         let selection = charList[charSelect-1];
-        console.log('Your Selected Character:');
+        console.log(gradient.cristal('Your Selected Character:'));
         printChar(selection);
        
           
@@ -67,23 +69,38 @@ async function charGet(user) {
     } catch(e){
       console.log(e);
     }
-    console.log('END OF THE GET FUNCTION');
-    // resolve();
-  })
-
-  
+  });
 };
 
 function printChar(selection) {
-   //TODO: change console log colors
-   console.log(`Name - ${selection.name}`);
-   console.log(`Race - ${selection.race}`);
-   console.log(`Class - ${selection.class}`);
-   console.log(chalk.green('Alignment - ') + `${selection.alignment}`);
-   console.log(`Deity - ${selection.deity}`);
-   console.log(`Proficiencies - \n\t${selection.proficient_skills.skill_1} \n\t${selection.proficient_skills.skill_2}`);
-   console.log(`Equipment - \n\tArmor: ${selection.equipment.armor}\n\tAdventure Pack: ${selection.equipment.adventure_packs}\n\tWeapons -\n\t\t 1: ${selection.equipment.weapons.choice_1}\n\t\t 2: ${selection.equipment.weapons.choice_2}`);
-   console.log(`Ability Scores - \n\tSTR: ${selection.ability_scores.str} | DEX: ${selection.ability_scores.dex} | CON: ${selection.ability_scores.con} | INT: ${selection.ability_scores.int} | WIS: ${selection.ability_scores.wis} | CHA: ${selection.ability_scores.cha}`);
+  let regexGood = /\b(\w*Good\w*)\b/gmi;
+  let regexEvil = /\b(\w*Evil\w*)\b/gmi;
+
+   console.log(gradient.cristal('Name -'), selection.name);
+   console.log(gradient.cristal('Race -'), selection.race);
+   console.log(gradient.cristal('Class -'), selection.class);
+
+  //  console.log(`Name - ${selection.name}`);
+  //  console.log(`Race - ${selection.race}`);
+  //  console.log(`Class - ${selection.class}`);
+
+  if (regexEvil.test(selection.alignment)) { 
+    console.log(gradient.cristal('Alignment -'), chalk`{redBright ${selection.alignment}}`);
+  } else if (regexGood.test(selection.alignment)) {
+    console.log(gradient.cristal('Alignment -'), chalk`{greenBright ${selection.alignment}}`);
+  } else {
+    console.log(gradient.cristal('Alignment -'), chalk`{blueBright ${selection.alignment}}`);
+  }
+
+  console.log(gradient.cristal('Deity -'), selection.deity);
+  console.log(gradient.cristal('Proficiencies -'), `\n\t${selection.proficient_skills.skill_1} \n\t${selection.proficient_skills.skill_2}`);
+  console.log(gradient.cristal('Equipment - ', '\n\tArmor: '), selection.equipment.armor, gradient.cristal('\n\tAdventure Pack: '), selection.equipment.adventure_packs, gradient.cristal('\n\tWeapons -\n\t\t 1: '), selection.equipment.weapons.choice_1, gradient.cristal('\n\t\t 2: '), selection.equipment.weapons.choice_2);
+  console.log(gradient.cristal('Ability Scores - \n\tSTR: '), selection.ability_scores.str, gradient.cristal(' | DEX: '), selection.ability_scores.dex, gradient.cristal(' | CON: '), selection.ability_scores.con, gradient.cristal(' | INT: '), selection.ability_scores.int, gradient.cristal(' | WIS: '), selection.ability_scores.wis, gradient.cristal(' | CHA: '), selection.ability_scores.cha)
+
+  //  console.log(`Deity - ${selection.deity}`);
+  //  console.log(`Proficiencies - \n\t${selection.proficient_skills.skill_1} \n\t${selection.proficient_skills.skill_2}`);
+  //  console.log(`Equipment - \n\tArmor: ${selection.equipment.armor}\n\tAdventure Pack: ${selection.equipment.adventure_packs}\n\tWeapons -\n\t\t 1: ${selection.equipment.weapons.choice_1}\n\t\t 2: ${selection.equipment.weapons.choice_2}`);
+  //  console.log(`Ability Scores - \n\tSTR: ${selection.ability_scores.str} | DEX: ${selection.ability_scores.dex} | CON: ${selection.ability_scores.con} | INT: ${selection.ability_scores.int} | WIS: ${selection.ability_scores.wis} | CHA: ${selection.ability_scores.cha}`);
 }
 
 
